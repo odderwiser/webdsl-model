33c5522ed2e49ce2363cc5f9d76f34f4410ab009
Aleksandra Wolska <wcqaguxa@gmail.com>
2024-01-26 13:49:06 +0100

    Condition Effect fixed condition value

--- src/Effects.hs
+++ src/Effects.hs
@@ -1,6 +1,7 @@
 module Effects where
 import Utils.Composition
 import Utils.Free
+import Bool.Syntax (LitB)
 
 data Operation p v k
   = Oper p [v] (v -> k)
@@ -9,8 +10,8 @@ data Operation p v k
 binaryOp :: Operation p v <: f => p -> v -> v -> Free f v
 binaryOp param e1 e2 = Op $ inj $ Oper param [e1, e2] Pure
 
-data Cond v k =  Cond v k k
+data Cond k =  Cond LitB k k
   deriving Functor
 
-cond :: Cond v <: f => v -> Free f a -> Free f a -> Free f a
+cond :: Cond <: f => LitB -> Free f a -> Free f a -> Free f a
 cond x m1 m2 = Op $ inj $ Cond x m1 m2
