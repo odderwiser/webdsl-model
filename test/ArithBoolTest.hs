6503b95ffba5b9ebdfb6aa308873f022a85d9183
Aleksander Wolska <odderwiser@gmail.com>
2024-02-09 13:21:40 +0100

    added test

--- test/ArithBoolTest.hs
+++ test/ArithBoolTest.hs
@@ -5,12 +5,12 @@ import Effects
 import Bool.Syntax as B
 import Utils.Composition
 import Arith.Syntax as A
-import Arith.Interface as A (denote) 
+import Arith.Interface as A (denote)
 import Test.HUnit
 import Utils.Handler
 import Bool.Handlers as B
 import Arith.Handlers as A
-import Bool.Interface as B (denote) 
+import Bool.Interface as B (denote)
 import Utils.Fix (bin)
 import Utils.Fix (Fix(In))
 import Utils.Fix (injF)
@@ -18,16 +18,16 @@ import Utils.Fix (injF)
 type Eff = Cond + Operation OpB LitB + Operation OpArith LitAr + End
 type V = Either LitB LitAr
 
-runBA :: (Env 
-  -> Free 
-    (Cond + Operation OpB LitB + Operation OpArith LitAr + End) 
-    (Either LitB LitAr)) 
+runBA :: (Env
+  -> Free
+    (Cond + Operation OpB LitB + Operation OpArith LitAr + End)
+    (Either LitB LitAr))
   -> Either Bool Int
-runBA e = 
-  case unwrap 
+runBA e =
+  case unwrap
     $ handle A.binOp
     $ handle B.binOp
-    $ handle condition 
+    $ handle condition
     $ e []
   of (Left (B.Lit val)) -> Left val
 
@@ -39,14 +39,27 @@ instance Denote Boolean Eff V where
 
 testIf :: Test
 testIf = TestCase (
-        assertEqual "add"
-        (Right 2)
-        (runBA $ foldD 
-        (In (inj $ If (injF (LitB (B.Lit False)))
-            (injF (LitAr (A.Lit 1))) 
-            (injF  (LitAr (A.Lit 2)) :: Fix (Arith + Boolean)))
-        ))
-    )
+  assertEqual "add"
+  (Right 2)
+  (runBA $ foldD
+  (injF $ If (injF (LitB (B.Lit False)))
+      (injF (LitAr (A.Lit 1)))
+      (injF  (LitAr (A.Lit 2)) :: Fix (Arith + Boolean))
+  ))
+  )
+
+testIfComp :: Test
+testIfComp = TestCase (
+  assertEqual "add"
+  (Right 1)
+  (runBA $ foldD
+  (injF $ If (injF (OpB
+      Or
+        (injF $ LitB (B.Lit False))
+        (injF $ LitB (B.Lit True))))
+      (injF (LitAr (A.Lit 1)))
+      (injF  (LitAr (A.Lit 2)) :: Fix (Arith + Boolean))
+  )))
 
 arithBoolTests :: Test
-arithBoolTests = TestList [testIf]
+arithBoolTests = TestList [testIf, testIfComp]
