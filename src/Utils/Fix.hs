5b6ee842e84a11c8fbabff0b0056f6d536b61f64
Aleksander Wolska <odderwiser@gmail.com>
2024-05-08 11:25:23 +0200

    Fix projection

--- src/Utils/Fix.hs
+++ src/Utils/Fix.hs
@@ -6,5 +6,8 @@ data Fix f = In (f (Fix f))
 injF :: (f <: g) => f (Fix g) -> Fix g 
 injF = In . inj
 
+projF :: (f <: g) => Fix g -> Maybe (f (Fix g))
+projF (In fix) = proj fix
+
 class (f <: g) => BinaryInject f g op where
     bin ::  op -> g (Fix g) -> g (Fix g) -> f (Fix g)
