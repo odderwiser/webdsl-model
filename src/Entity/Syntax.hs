d8e80744294b20671d84685ea4dc04ea1ba25fbe
Aleksander Wolska <odderwiser@gmail.com>
2024-05-13 00:16:56 +0200

    syntax for entities

--- src/Entity/Syntax.hs
+++ src/Entity/Syntax.hs
@@ -6,7 +6,12 @@ type EName = String
 type PName = String
 
 data Entity e = Entity EName (Maybe (Entity e)) [PName] [FDecl e]
-
+    deriving Functor
+    
 type EntityEnv = [(PName, Address)]
 
 data Dot e = Param EName PName
+
+data Program e
+    = Program [FDecl e] [Entity e] e
+    deriving Functor
